b3fccd9affdf5981ad54035642eb7fcf9629b487
Paul-Louis <paullouismasca@gmail.com>
2021-02-06 18:21:36 -0500

    Added bassic navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :data-vue-component="$options.name">
+    <b-navbar type="is-light" shadow>
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ name: 'HomePage' }">
+          <b-icon icon="home" size="is-medium" />
+
+          <span>
+            Shifts Manager
+          </span>
+        </b-navbar-item>
+      </template>
+
+      <template #end>
+        <b-navbar-item tag="div">
+          <router-link v-if="isAuthenticated" :to="{ name: 'Auth/SignOut' }">
+            Sign-out
+          </router-link>
+
+          <div v-else class="buttons">
+            <router-link class="button is-primary" :to="{ name: 'Auth/SignUp' }">
+              Sign-up
+            </router-link>
+
+            <router-link class="button is-dark" :to="{ name: 'Auth/LogIn' }">
+              Log-in
+            </router-link>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'NavBar',
+
+  computed: {
+    isAuthenticated(): boolean {
+      return !!this.$store.state.auth.currentUser;
+    },
+  },
+});
+</script>
--- src/views/auth/AuthHome.vue
+++ src/views/auth/AuthHome.vue
@@ -1,5 +1,7 @@
 <template>
   <div :data-vue-component="$options.name">
+    <nav-bar />
+
     <div v-if="$router.getMatchedComponents().pop().name === $options.name">
       <!-- Nothing to see here -->
     </div>
@@ -11,7 +13,14 @@
 <script lang="ts">
 import Vue from 'vue';
 
+// Components
+import NavBar from '@/components/NavBar.vue';
+
 export default Vue.extend({
   name: 'AuthHome',
+
+  components: {
+    NavBar,
+  },
 });
 </script>
