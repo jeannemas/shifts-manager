79d62012bd0b6c501fc227968b7e9a6806b55d9e
Paul-Louis Mas <paullouismasca@gmail.com>
2021-03-27 21:58:07 -0400

    Added Vuetify

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ import App from './App.vue';
 
 // Bootstraps scripts
 import '@/bootstraps';
+import vuetify from './bootstraps/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
