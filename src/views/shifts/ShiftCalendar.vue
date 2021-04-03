975c6c7451997cdcec4fe4c1cd835c8baff76c20
Paul-Louis Mas <paullouismasca@gmail.com>
2021-04-03 14:43:51 -0400

    Improving calendar

--- src/views/shifts/ShiftCalendar.vue
+++ src/views/shifts/ShiftCalendar.vue
@@ -1,12 +1,25 @@
 <template>
-  <div :data-vue-component="$options.name" style="min-height: 600px;">
-    <v-calendar
-      v-model="dateFocus"
-      :weekdays="[1, 2, 3, 4, 5, 6, 0]"
-      type="month"
-      event-overlap-mode="stack"
-      :events="events"
-    />
+  <div :data-vue-component="$options.name">
+    <div class="block">
+      <div class="buttons">
+        <b-button icon-left="chevron-left" @click="() => $refs.calendar.prev()" />
+
+        <b-button icon-left="chevron-right" @click="() => $refs.calendar.next()" />
+      </div>
+    </div>
+
+    <v-sheet height="600">
+      <v-calendar
+        ref="calendar"
+        :value="now"
+        :now="now"
+        :weekdays="[1, 2, 3, 4, 5, 6, 0]"
+        type="week"
+        event-overlap-mode="stack"
+        :events="events"
+        :event-color="getEventColor"
+      />
+    </v-sheet>
   </div>
 </template>
 
@@ -29,7 +42,11 @@ export default Vue.extend({
 
   data() {
     return {
-      dateFocus: new Date(),
+      now: ((date) =>
+        `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date
+          .getDate()
+          .toString()
+          .padStart(2, '0')}`)(new Date()),
     };
   },
 
@@ -49,8 +66,13 @@ export default Vue.extend({
           start: new Date(startTime as number),
           end: new Date(endTime as number),
           color: 'blue',
+          timed: true,
         }));
     },
+  },
+
+  methods: {
+    getEventColor: ({ color }: CalendarEvent) => color,
   },
 });
 </script>
